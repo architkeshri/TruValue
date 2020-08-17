35c102c8e41cefb77b65332b9ed453757d5ad5e4
Archit Keshri <architkeshri4@gmail.com>
2020-08-17 15:06:08 +0530

    Updated settings

--- TruValue/settings.py
+++ TruValue/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'TruValue.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [os.path.join(BASE_DIR,'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
