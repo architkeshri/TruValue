97b656f06567fc1fc2c7772a744bd4ceeee08389
Archit Keshri <architkeshri4@gmail.com>
2020-08-17 14:50:08 +0530

    Updated settings

--- TruValue/settings.py
+++ TruValue/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iqaj$0_qpk7in^3-tt)t^uer*%)!quh#3%uj&--c0yn0xmy!7x'
+#SECRET_KEY = 'iqaj$0_qpk7in^3-tt)t^uer*%)!quh#3%uj&--c0yn0xmy!7x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['truvalue.herokuapp.com']
 
 
 # Application definition
